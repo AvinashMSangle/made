caa139449d461a979391fed6f03f15621d785a6d
Avinash Sangle <avinashMS.3119@proton.me>
2025-02-04 18:14:57 +0000

    update Input and SignUp Component

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
-import express from "express";
 import http from "http";
+import express from "express";
 import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -13,15 +18,17 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`New client connected: ${socket.id}`);
 
-  socket.on("emoji", (data) => {
-    socket.io.emit("newEmoji", data);
+  socket.on("new_user", (data) => {
+    console.log("New user joined our chat room", data);
+
+    socket.broadcast.emit("user_joined", data);
+
+    // io.emit (sab ke pass)
+    // socket.emit (jisne new user diya tha uske pass only)
+    // socket.broadcast.emit (jisne new user diya tha usko chodh kar baki sab)
   });
 });
 
-app.get("/", (req, res) => {
-  res.send("Server is up and running");
-});
-
 server.listen(8000, () => {
-  console.log("Server is listening on port 8000");
+  console.log("Server is up and running");
 });
